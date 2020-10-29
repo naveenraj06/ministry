9efc8752e7a176e6bdf77ed8fb33b59b0049990b
naveenraj06 <naveenrajiv.06@gmail.com>
2020-10-29 14:35:13 +0530

    new commit

---
+++ assets/js/main.js
@@ -0,0 +1,55 @@
+$(function(){
+    $(window).on('scroll', function(){
+        headerAnimate();
+    });  
+    headerAnimate();  
+    function headerAnimate() {
+        var _top = $(window).scrollTop() + 58,
+        _vh = window.innerHeight;
+        if(_top >= _vh){
+            $('header').addClass('active');
+        } else if (window.innerWidth <= 768) {
+            $('header').addClass('active');
+        }else {
+            $('header').removeClass('active');
+        }
+    }    
+    $('.menu-list a').on('click', function(e){
+        e.preventDefault();
+        _this = $(this);
+        _target = _this.attr('href');
+        _top = $(_target).offset().top;
+        $('html,body').animate({
+            scrollTop: _top - 40
+        },1500);
+        if (window.innerWidth <= 768) {
+            $('.menu-list_wrapper').toggleClass('active').fadeToggle('slow');
+        }        
+    });    
+    $('.hamburger').on('click', function(){
+        $('.menu-list_wrapper').toggleClass('active').fadeToggle('slow');
+    });
+    $('#contact-form').on('submit', function(e){
+        e.preventDefault();
+        var _this = $(this);
+        _this.find('input').each(function(){
+        var _val = $(this).val();
+        
+            if(_val=="" || _val==undefined ){
+                $('#error').show();
+                return false;
+            } else {
+                $('#error').hide();
+            }
+        });
+    });     
+    $('#contact-form input').on('blur', function(e){
+            var _val = $(this).val();
+            if(_val==""){
+                $('#error').show();
+                return false;
+            } else {
+                $('#error').hide();
+            }
+    });   
+})
